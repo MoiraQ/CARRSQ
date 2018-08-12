d37fff6c65f69c4f80af4465a15ac985d8cbd45c
bscullett <benjamin.scullettdean@connect.qut.edu.au>
2018-08-12 18:34:46 +1000

    added click event distance increment button

--- Capstone/app/src/main/java/com/example/momo/capstone/MainActivity.java
+++ Capstone/app/src/main/java/com/example/momo/capstone/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.momo.capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button disPlusBtn = (Button) findViewById(R.id.distPlusBtn);
+        disPlusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //action
+                TextView distText = (TextView) findViewById(R.id.distText);
+
+                int distInt = Integer.parseInt(distText.getText().toString());
+                int distIntNew = distInt + 1;
+
+                distText.setText(distIntNew + "");
+            }
+        });
     }
 }
